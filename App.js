a09af7901e1b7f8c549db1106daf58898b9eae23
keithcheung <keith.cheung41@gmail.com>
2018-09-16 00:08:56 -0400

    Include navigation

--- App.js
+++ App.js
@@ -1,54 +1,17 @@
 import React from 'react';
 import firebase from 'firebase';
-import { Button } from 'react-native-elements';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Lobby from './components/Lobby';
+import Prelobby from './components/Prelobby';
+import { createStackNavigator } from 'react-navigation';
+
+const RootStack = createStackNavigator({
+  Prelobby: { screen: Prelobby },
+  Lobby: { screen: Lobby }
+});
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-
-    var config = {
-      apiKey: 'AIzaSyBD970a5O3FYQsvmg48dbXzEX-3m2se3So',
-      authDomain: 'storybox-hackmit.firebaseapp.com',
-      databaseURL: 'https://storybox-hackmit.firebaseio.com',
-      projectId: 'storybox-hackmit',
-      storageBucket: 'storybox-hackmit.appspot.com',
-      messagingSenderId: '716758062079'
-    };
-    firebase.initializeApp(config);
-
-    this.state = {
-      name: ''
-    };
-  }
-  joinLobby() {
-    return <Lobby />;
-  }
   render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={{ height: 80, fontSize: 26 }}
-          placeholder="Type your name here"
-          onChangeText={name => this.setState({ name })}
-        />
-        <Button
-          buttonStyle={{ backgroundColor: 'green' }}
-          title="Join Room"
-          underlayColor="red"
-          onPress={this.joinLobby}
-        />
-      </View>
-    );
+    return <RootStack />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.6)',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
--- components/Lobby.js
+++ components/Lobby.js
@@ -1,5 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import firebase from 'firebase';
+import { Dropdown } from 'react-native-material-dropdown';
 
-const Title = () => <Text>Hello Title</Text>;
-export default Title;
+export default class Lobby extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      language: 'Javascript'
+    };
+  }
+  render() {
+    let data = [
+      {
+        value: 'Happy'
+      },
+      {
+        value: 'Sad'
+      },
+      {
+        value: 'Angry'
+      }
+    ];
+    const { navigation } = this.props;
+    const name = navigation.getParam('name');
+    return (
+      <View>
+        <Text style={{ fontSize: 40, textAlign: 'center' }} color="#000">
+          {name}
+        </Text>
+        <Text style={styles.waiting}>Waiting for players...</Text>
+        <Dropdown label="Sentiment" data={data} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  waiting: {
+    fontSize: 30,
+    paddingTop: 30,
+    justifyContent: 'center',
+    alignSelf: 'center'
+  }
+});
